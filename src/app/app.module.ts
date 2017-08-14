bec15d9be2fded7a5c194f7dc5c37b9f7fc764e5
chance <chancemagno@gmail.com>
2017-08-14 14:21:12 -0700

    add parallax to app.main.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MaterializeModule } from 'angular2-materialize';
+import * as $ from 'jquery';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
